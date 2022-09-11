a7a817f9607fcc0efea0c4b4b2a64eedac15b45a
momohrasheed06 <rashsmooth@yahoo.com>
2022-09-11 06:07:32 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+	char school;
+
+	for (school = 'a'; school <= 'z'; school++)
+		putchar(school);
+	for (school = 'A'; school <= 'Z'; school++)
+		putchar(school);
+	putchar('\n');
+	return (0);
+}
