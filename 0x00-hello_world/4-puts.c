c1ba9b3dcbf84ad9a003954b2273565f8ad4e882
Abdulrasheed <rashysmooth@yahoo.com>
2022-09-11 02:13:02 +0100

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-        char test_str[] = "\"Programming is like building a multilingual puzzle"
+        char test_str[] = "\"Programming is like building a multilingual puzzle";
 
         puts(test_str);
         return (0);
