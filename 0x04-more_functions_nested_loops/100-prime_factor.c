6bc19408c8bbc68dc6f39c04f17cd3f507dadb7e
Abdulrasheed <rashysmooth@yahoo.com>
2022-09-16 01:35:53 +0100

    Task 11

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,29 +1,32 @@
-#include "main.h"
-
-
-
-/**
-
- * _isupper - function that checks for uppercase character.
-
- * @c: int type number
-
- * Return: 1 if c is uppercase, 0 otherwise
-
- */
-
-
-
-int _isupper(int c)
-
-{
-
-if (c >= 65 && c <= 90)
-
-return (1);
-
-else
-
-return (0);
-
-}
+#include <stdio.h> 
+ #include <math.h> 
+  
+ /** 
+  * main - prints the largest prime factorial of 612852475143 
+  * 
+  * Return: 0 
+  */ 
+ int main(void) 
+ { 
+         long int max = -1, n = 612852475143; 
+         int i = 3; 
+  
+         while (n % 2 == 0) 
+         { 
+                 max = 2; 
+                 n /= 2; 
+         } 
+         while (i <= sqrt(n)) 
+         { 
+                 while (n % i == 0) 
+                 { 
+                         max = i; 
+                         n = n / i; 
+                 } 
+                 i += 2; 
+         } 
+         if (n > 2) 
+                 max = n; 
+         printf("%ld\n", max); 
+         return (0); 
+ }
